29b1340473abe2027828bcfefcde83453fc01eb3
hivefive <hpdobrica@gmail.com>
2020-03-27 17:30:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,53 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Penta Pets
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-uploader">
+        <p>Working from home? Send us a picture of your pets!</p>
+        <label className="upload-btn">
+          Upload
+          <input type="file" name="myImage" accept="image/x-png,image/gif,image/jpeg" />
+        </label>
+
+      </div>
+      <div className="App-gallery">
+        <div className="App-image-container">
+          <img src="img/a.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/b.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/c.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/d.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/e.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/f.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/g.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/h.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/i.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/j.jpg" className="App-image"></img>
+        </div>
+        <div className="App-image-container">
+          <img src="img/k.jpg" className="App-image"></img>
+        </div>
+      </div>
     </div>
   );
 }
